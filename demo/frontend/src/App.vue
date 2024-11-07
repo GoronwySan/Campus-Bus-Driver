908b499289f4690cc982990b4c2c11733f03e2a3
goronwy <1216457132aa@gmail.com>
2024-11-07 19:18:45 +0800

    first commit

---
+++ demo/frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>我的高德地图</h1>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <MapComponent />
+  </div>
+</template>
+
+<script>
+import MapComponent from './components/Map.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MapComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
